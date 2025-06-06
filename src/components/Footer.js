5e679e990ce498cbc73bca07f960e54f10e5989f
lyanaTougon <lyana.tougon@gmail.com>
2025-06-06 16:05:54 +0200

    fix: add logo in footer + finissions du footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,32 +2,37 @@ import "./Footer.css"
 const Footer = ( ) => {
   return ( 
   
-  <footer>
-        <div class="container">
-        <div class="footer-content">
-          <div class="footer-up">
-           <p>Email: jujitsu.stephane@live.fr</p>
-           <p>Créé par: Tougon Eddy</p>
-           <p><a>Mention legale</a></p>
-           </div>
-           <div class="social-icons">
-      <a href="https://www.facebook.com/CgtDhlExpressIntl" target="_blank" class="facebook">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="https://chat.whatsapp.com/Bpp6yRJDqyL0ENFxmC23iq" target="_blank" class="whatsapp">
-        <i class="fab fa-whatsapp"></i>
-      </a>
-            </div>
-            
-            <div class="footer-bottom">
-              © 2025 Tous droits réservés
-              </div>
-
-          </div>
-          
+<footer>
+  <div class="container">
+    <div class="footer-content">
+      <div class="footer-up">
+        <div class="footer-logo">
+          <img src="/static/media/CGT transport.3fa524beae51b295fe42.png" alt="Logo CGT" />
         </div>
-        
-      </footer>
+
+        <div class="footer-text">
+          <p>Email: jujitsu.stephane@live.fr</p>
+          <p>Créé par: Tougon Eddy</p>
+          <p><a href="#">Mention légale</a></p>
+        </div>
+
+        <div class="social-icons">
+          <a href="https://www.facebook.com/CgtDhlExpressIntl" target="_blank" class="facebook">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://chat.whatsapp.com/Bpp6yRJDqyL0ENFxmC23iq" target="_blank" class="whatsapp">
+            <i class="fab fa-whatsapp"></i>
+          </a>
+        </div>
+      </div>
+
+      <div class="footer-bottom">
+        © 2025 Tous droits réservés
+      </div>
+    </div>
+  </div>
+</footer>
+
   )
 }
 export default Footer;
